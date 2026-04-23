41d4c46707b8d4e4a5ac64e17b88ac7ffeeffb0c
WJWJWJ2002 <157001458+WJWJWJ2002@users.noreply.github.com>
2026-04-23 19:04:14 +0800

    Top module, Memory storage module, Window generation module and paramters header file

---
+++ top.v
@@ -0,0 +1,46 @@
+module top(clk, new_pix);
+	`include "parameters.vh"
+	input clk;
+	output[DATA_WIDTH-1:0] new_pix;
+	wire clk, gen_req, done_init_buf, data_valid, rden_rom, done_filt, done_gen;
+	wire[DATA_WIDTH-1:0] rom_out_ff, fifo2_out_ff, fifo1_out_ff, row, col;
+	reg init_buff;
+
+	fifo_rom memory_buffer (.clk(clk), ,init_buff(init_buff), .gen_req(gen_req), .rden_rom(rden_rom), 
+	.rom_out_ff(rom_out_ff), .fifo1_out_ff(fifo1_out_ff), .fifo2_out_ff(fifo2_out_ff), 
+	.data_valid(data_valid), .done_init_buf(done_init_buf)
+	);
+
+	win_gen window_generation (.gen_ready(done_init_buf), .gen_req(gen_req), .clk(clk), .rden_rom(rden_rom),
+	.done_gen(done_gen), .done_filt(done_filt), .data_valid(data_valid),
+	.row1_out_ff(fifo2_out_ff), .row2_out_ff(fifo1_out_ff), .rom_out_ff(rom_out_ff), .row(row), .col(col)
+	.p1(p1), .p2(p2), .p3(p3), .p4(p4), .p5(p5), .p6(p6), .p7(p7), .p8(p8), .p9(p9)
+	);
+
+//	filter filter_module (.clk(clk), .done_gen(done_gen), 
+//	.p1(p1), .p2(p2), .p3(p3), .p4(p4), .p5(p5), .p6(p6), .p7(p7), .p8(p8), .p9(p9),
+//	.done_filt(done_filt), .new_pix(new_pix)
+//	);
+
+	initial begin
+		init_buff = 1'b0;
+		row = 8'd0;
+		col = 8'd0;
+	end
+
+	always @(posedge clk) begin
+		if (initial_delay < 3'd7) begin
+			init_buff <= 1'b0;
+			initial_delay <= initial_delay + 1'b1;
+		end
+		else begin
+			init_buff <= 1'b1;
+			initial_delay <= initial_delay;
+			if (done_filt) begin
+				row <= row + 1'b1;
+				col <= col + 1'b1;
+			end
+		end
+	end
+endmodule
+
---
+++ win_gen.v
@@ -0,0 +1,119 @@
+module win_gen(gen_ready, gen_req, done_gen, done_filt, rden_rom, clk, row1_out_ff, row2_out_ff, rom_out_ff, row, col
+	p1, p2, p3, p4, p5, p6, p7, p8, p9
+);
+	`include "parameters.vh"
+	input clk, gen_ready, done_filt; //gen_ready = done_init_buf
+	input[DATA_WIDTH-1:0] row1_out_ff, row2_out_ff, rom_out_ff;
+	output gen_req, done_gen, rden_rom;
+	output[DATA_WIDTH-1:0] p1, p2, p3, p4, p5, p6, p7, p8, p9, row, col;
+	wire done_gen, rden_rom;
+	reg gen_req;
+	reg[1:0] counter;
+	reg[2:0] state, next_state;
+	reg[DATA_WIDTH-1:0] p1, p2, p3, p4, p5, p6, p7, p8, p9;
+	localparam[7:0] MAX_COL = 8'd255, MAX_ROW = 8'd255;
+	localparam[2:0] WAIT_INIT = 3'd0, S1 = 3'd1, WAIT = 3'd2, S2 = 3'd3 DONE_GEN = 3'd4, WAIT_FILT = 3'd5;
+	
+	// Next state combinational logic
+	always @(*) begin
+		case (state)
+			WAIT_INIT: next_state = (gen_ready) ? (S1) : (WAIT_INIT);
+			S1: next_state = WAIT;
+			WAIT: next_state = (data_valid || (col == 8'd0 && counter == 2'd0) || 
+				(col == MAX_COL && counter == 2'd2)) ? (S2) : (WAIT);
+			S2: next_state = (counter < 2'd2) ? (WAIT) : (DONE_GEN);
+			DONE_GEN: next_state = WAIT_FILT;
+			WAIT_FILT: next_state = (done_filt) ? ((col == 8'd0) ? (S1) : (S2)) : (WAIT_FILT);
+			default: next_state = WAIT_INIT;
+		endcase
+	end
+	// WIndow generation
+	always @(posedge clk) begin
+		if (next_state == S2) begin
+			if (data_valid) begin
+				p9 <= rom_out_ff;
+				p8 <= p9;
+				p7 <= p8;
+				p6 <= row2_out_ff;
+				p5 <= p6;
+				p4 <= p5;
+				p3 <= row1_out_ff;
+				p2 <= p3;
+				p1 <= p2;
+			end
+			else begin
+				p9 <= 8'd0;
+				p8 <= p9;
+				p7 <= p8;
+				p6 <= 8'd0;
+				p5 <= p6;
+				p4 <= p5;
+				p3 <= 8'd0;
+				p2 <= p3;
+				p1 <= p2;
+			end
+		end
+	end
+
+	// State output for registers (Counters and pixel tracking)
+	always @(posedge clk) begin
+		case (state)
+			WAIT_INIT: begin
+				counter <= 2'd0;
+				col <= 8'd0;
+				row <= 8'd0;
+			end
+			S2: begin
+				counter <= counter + 1'b1;
+			end
+			DONE_GEN: begin
+				counter <= (col < MAX_COL) ? (counter - 1'b1) : (8'd0);
+				col <= col + 1'b1;
+				row <= (col == MAX_COL) ? (row + 1'b1) : row;
+			end
+			default: begin
+				counter <= counter;
+				row <= row;
+				col <= col;
+				p1 <= p1;
+				p2 <= p2;
+				p3 <= p3;
+				p4 <= p4;
+				p5 <= p5;
+				p6 <= p6;
+				p7 <= p7;
+				p8 <= p8;
+				p9 <= p9;
+			end
+		endcase
+	end
+
+	// Load next state into state register
+	always @(posedge clk) begin
+		state <= next_state;
+	end
+
+	// gen_req output using always_comb block
+	always @(*) begin
+		case (state)
+			WAIT_INIT: gen_req = 1'b0;
+			S1: begin
+				gen_req = (col == 8'd0) ? (1'b1) : (1'b0);
+			end
+			WAIT: gen_req = 1'b0;
+			S2: begin
+				gen_req = (counter == 2'd1 && col == MAX_COL) ? (1'b0) : 
+					((counter == 2'd2) ? (1'b0) : (1'b1));
+			end
+			DONE_GEN: gen_req = 1'b0;
+			WAIT_FILT: gen_req = 1'b0;
+			default: gen_req = 1'b0;
+		endcase
+	end
+
+	// ROM read-enable and window generation finish signals output logic
+	assign rden_rom = (row == 8'd0 || row == MAX_ROW) ? (1'b0) : (1'b1);
+	assign done_gen = (state == DONE_GEN) ? (1'b1) : (1'b0);
+
+endmodule
+
